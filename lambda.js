f1e9ef439dae2f42d5df67065db2e0c05d1b734c
Miller Barahona <mbarahona@robinfood.com>
2021-11-07 16:45:57 -0500

    adding function lambda

--- lambda.js
+++ lambda.js
@@ -4,33 +4,33 @@ const crypto = require("crypto");
 const generarId = () => crypto.randomBytes(16).toString("hex");
 
 exports.handler = async (event) => {
-    
-    try {
-        let form = event.body;
-        let params = {
-            TableName: 'countries',
-            Item: {
-                countries_id: generarId(),
-                name: form.name,
-                country: form.country
-            }
-        };
-        
-        const data = await documentClient.put(params).promise();
-        const response = {
-            statusCode: 200,
-            body:JSON.stringify({
-                text: 'Hecho!!'
-            })
-        };
-        return response;
-    } catch (e) {
-        console.log(e)
-        const response = {
-        statusCode: 400,
-        body: JSON.stringify('Ha ocurrido un erorr!!'),
-        };
-        return response;
-    }
+
+  try {
+    let form = event.body;
+    let params = {
+      TableName: 'countries',
+      Item: {
+        countries_id: generarId(),
+        name: form.name,
+        country: form.country
+      }
+    };
+
+    const data = await documentClient.put(params).promise();
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        text: 'Hecho!!'
+      })
+    };
+    return response;
+  } catch (e) {
+    console.log(e)
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify('Ha ocurrido un erorr!!'),
+    };
+    return response;
+  }
 
 };
